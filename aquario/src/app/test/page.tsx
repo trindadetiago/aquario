ad6b1ab28bc12e1b66fbd30ff07c71386abe497c
Ian <ianbittencourt03@gmail.com>
2024-08-20 16:42:06 -0300

    feat: add ComponentWithCss-Ian

--- aquario/src/app/test/page.tsx
+++ aquario/src/app/test/page.tsx
@@ -1,14 +1,15 @@
+"use client";
+
 import ComponentWithTailwind from "../../Tests & Examples/ComponentWithTailwind";
 import ComponentWithShadcn from "../../Tests & Examples/ComponentWithShadcn";
 import ComponentWithCss from "../../Tests & Examples/ComponentWithCss/ComponentWithCss";
+import UserCardIan from "@/Tests & Examples/ComponentWithCSS-Ian/UserCardIan";
 
 export default function Test() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <p>Test Page</p>
-        <ComponentWithTailwind />
-        <ComponentWithShadcn />
-        <ComponentWithCss />
+        <UserCardIan/>
       </main>
     );
 }
