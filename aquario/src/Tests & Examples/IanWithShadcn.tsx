28aa139f718ae568e9890decc72aab85cd97963c
Ian <ianbittencourt03@gmail.com>
2024-08-21 13:52:06 -0300

    feat add: final shadcn

--- aquario/src/Tests & Examples/IanWithShadcn.tsx
+++ aquario/src/Tests & Examples/IanWithShadcn.tsx
@@ -15,31 +15,31 @@ import {
 
 const IanWithShadcn: React.FC = () => {
   return (
-        <Card>
-    <CardContent>
-    <div className="grid grid-cols-[100px_1fr_200px] w-[1000px]">
-       {/* image */}
-        <div className='flex justify-center items-center'>
-            <Image width={60} height={60} className="w-[60px] h-[60px] rounded-full" src={ian.src} alt="profile picture" />
-        </div>
-        {/* name */}
-        <div className='flex flex-col justify-start items-start'>
-            {/* upperbox */}
-            <div className='flex flex-row justify-end items-end mt-4 font-sans pt-[15px]'>
-                <h2 className="text-2xl font-light pr-[10px]">Ian Rocha Bittencourt</h2>
-                <p className="flex justify-center items-center bg-[#b8b8b8] text-base text-white rounded-lg w-[40px] h-[20px] mb-[3px]">PES</p>
+    <Card className='w-full min-w-[500px]'>
+        <CardContent className='p-0'>
+            <div className="grid grid-cols-[100px_1fr_200px] w-full">
+            {/* image */}
+                <div className='flex justify-center items-center'>
+                    <Image width={60} height={60} className="w-[60px] h-[60px] rounded-full" src={ian.src} alt="profile picture" />
+                </div>
+                {/* name */}
+                <div className='flex flex-col justify-start items-start'>
+                    {/* upperbox */}
+                    <div className='flex flex-row justify-end items-end mt-4 font-sans pt-[15px]'>
+                        <h2 className="text-2xl font-light pr-[10px]">Ian Rocha Bittencourt</h2>
+                        <p className="flex justify-center items-center bg-[#b8b8b8] text-base text-white rounded-lg w-[40px] h-[20px] mb-[3px]">PES</p>
+                    </div>
+                    {/* lowerbox */}
+                    <div className='flex justify-center items-center text-base font-normal text-[#726e6e] font-sans mb-20px'>
+                        <p className='mt-[5px] mb-[20px]'>Ciência da Computação</p>
+                    </div>
+                </div>
+                {/* button */}
+                <div className='flex justify-center items-center'>
+                <Button className='w-[100px] rounded-[1.2rem]'>Site</Button>
+                </div>
             </div>
-            {/* lowerbox */}
-            <div className='flex justify-center items-center text-base font-normal text-[#726e6e] font-sans mb-20px'>
-                <p className='mt-[5px] mb-[20px]'>Ciência da Computação</p>
-            </div>
-        </div>
-        {/* button */}
-        <div className='flex justify-center items-center'>
-        <Button className='w-[100px]'>Site</Button>
-        </div>
-    </div>
-    </CardContent>
+        </CardContent>
     </Card>
 
   );
