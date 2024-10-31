0173df90083412a6d330227806f6b232b8415f01
Emysntts <emylesantos.07@gmail.com>
2024-10-31 14:21:32 -0300

    add Banner and SearchFilters for repo

--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -4,56 +4,27 @@
 import GradientHeaderComponent from "@/components/Shared/gradient-header";
 import TypeBadge from "@/components/Shared/Badges";
 import UserCard from "@/components/Shared/UserCard";
+import SearchFilters from "@/components/Shared/SearchFilters";
+import Banner from "@/components/Shared/Banner";
 
 export default function Test() {
     const handleButtonClick = () => {
       alert('Button clicked!');
-    };
+    };  
 
     return (
       <div className="space-y-6 bg-gray-50 dark:bg-black min-h-screen flex flex-col">
-
-      <GradientHeaderComponent 
-          academicCenter="Centro de Informática" 
-          courses={["Ciência da Computação", "Engenharia da Computação", "Ciências de Dados e Inteligência Artificial"]} 
-          currentCourse="Ciência da Computação"
-      />  
-      <UserCard
-        name="Tiago Trindade de Oliveira"
-        profilePicture= "/fotoTiago.jpeg" 
-        major="Ciência da Computação"
-        type="pessoa"
-        site="site" 
+       
+      <div className="pt-28">
+      <Banner 
+        title="Explore vagas de emprego, estágio e de projetos voluntários no CI e afora"
+        description="Nosso mural de vagas permite que qualquer pessoa ou laboratório busque alunos interessados em vagas em projetos ou estágios."
+        buttonText="Divulgar uma vaga"
+         // link do botão
       />
-      
-      <div>
-        <div className="flex gap-10 m-2">
-          <TypeBadge type="laboratorio" size="small" /> 
-          <TypeBadge type="laboratorio" size="large" />
-        </div>
-
-        <div className="flex gap-10 m-2">
-          <TypeBadge type="pessoa" size="small" />
-          <TypeBadge type="pessoa" size="large" />
-        </div>  
+      </div>
+      <SearchFilters/>
         
-        <div className="flex gap-10 m-2">
-          <TypeBadge type="oficial" size="small" />
-          <TypeBadge type="oficial" size="large" />
-        </div>
-          
-        <div className="flex gap-10 m-2">
-          <TypeBadge type="grupo" size="small" />
-          <TypeBadge type="grupo" size="large" />
-        </div>
-        
-        <div className="flex gap-10 m-2">
-          <TypeBadge type="externo" size="small" />
-          <TypeBadge type="externo" size="large" />
-        </div>
-
-      </div> 
-
       </div>
   
       
